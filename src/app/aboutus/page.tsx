ddabfc154abc2685679bc370d63e38a0e75a4bf3
Pariodus <145102769+Pariodus@users.noreply.github.com>
2024-03-24 09:24:37 +0700

    about us

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -3,9 +3,13 @@ import AboutUs from "@/components/AboutUs";
 export default function Aboutus(){
     return (
         <main className="w-[100%] flex flex-col items-center space-y-4">
-            <AboutUs github="Pariodus" discord="Pariodus"/>
-            <AboutUs github="dffdgd" discord="AaRaiWaa"/>
-            <AboutUs github="Pariodus" discord="N"/>
+            <div className="text-4xl mt-5">Contact Us</div>
+            <div className="items-center">
+                <AboutUs github="Pariodus" discord="Pariodus"/>
+                <AboutUs github="Nisha11045" discord="AaRaiWaa"/>
+                <AboutUs github="Nitchakan04" discord="N"/>
+            </div>
+            
         </main>
     )
 }
--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,13 +2,23 @@ import Image from "next/image"
 
 export default function AboutUs({github, discord}: {github:string, discord:string}){
     return (
-        <div className="flex flex-col my-5">
-            {/* <Image src="/img/gitLogo.jpg"/> */}
-            <div>{github}</div>
+        <div className="flex flex-col m-5 text-left">
+            <div className="flex flex-row items-center m-7">
+                <Image src="/img/gitLogo.jpg"
+                    alt='GitHub Image'
+                    width={0} height={0} sizes="100vw"
+                    className="rounded-lg  w-[100px]"/>
+                <div className="m-2">{github}</div>
+            </div>
             
-
-            <div>{discord}</div>
-            
+            <div className="flex flex-row items-center">
+                <Image src="/img/discordLogo.jpg"
+                    alt='Discord Image'
+                    width={0} height={0} sizes="100vw"
+                    className="rounded-lg ml-9 w-[80px]"/>
+                <div className="ml-5">{discord}</div>
+            </div>
+                        
         </div>
     )
 }
